0b829bc7f4f1efe58f71a1d2829a84bbdbcee152
Caleb MacDonald Black <calebmacdonaldblack@gmail.com>
2018-02-19 08:31:00 +1000

    Fixed bug with outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "ses_verification_token" {
-  value = "${aws_ses_domain_identity.ses.domain}"
+  value = "${aws_ses_domain_identity.ses.verification_token}"
 }
 
 output "ses_arn" {
-  value = "${aws_ses_domain_identity.ses.verification_token}"
+  value = "${aws_ses_domain_identity.ses.domain}"
 }
